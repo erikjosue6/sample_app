2684fe4d003285a0b43ce8fffc01dc3fec0b8dc9
Erik J. Santiago <erik.santiago@cemi-tech.com>
2013-08-12 22:47:17 -0400

    Finish user edit, update, index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
   
   def show
   	@user = User.find(params[:id])
@@ -17,6 +19,38 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end  
+
+  def edit
+    #@user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
+
+  private 
+
+  def signed_in_user
+    unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+    end     
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user) 
   end
 end
   	
